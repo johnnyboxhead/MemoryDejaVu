2eb3c3dafcd9b1a99e5c9b481af91aa6b9b4a5e9
Johnny <johnnyheath@hotmail.com>
2018-06-11 22:41:58 -0400

    Added logic to the game to get the waves to render randomly

--- memorydejavu/src/App.js
+++ memorydejavu/src/App.js
@@ -6,15 +6,27 @@ import waves from "./waves.json"
 import Wrapper from "./components/Wrapper"
 import Title from "./components/Title"
 
+// let randomWave = Math.floor(Math.random() * waves.length)
+// console.log(randomWave)
+
+//https://stackoverflow.com/questions/38101522/how-to-render-random-objects-from-an-array-in-react?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+function shuffleArray(array) {
+  let i = array.length - 1;
+  for (; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
+}
+
 class App extends Component {
   state = {
     waves
   }
-  
+
   selectWave = id => {
-    // Filter this.state.friends for friends with an id not equal to the id being removed
-    // const waves = this.state.waves.filter(wave => wave.id !== id);
-    // Set this.state.friends equal to the new friends array
     this.state.waves.map(wave => {
       if (wave.id === id){
         if (wave.clicked === true) {
@@ -27,14 +39,16 @@ class App extends Component {
     this.setState({ waves });
   };
 
-  
+    
 
   render() {
+    const shuffledWaves = shuffleArray(waves);
     return (
       <Wrapper>
         <Title>Friends List</Title>
-        {this.state.waves.map(wave => (
+        {shuffledWaves.map(wave => (
           <Wave
+            randomWave={this.randomWave}
             selectWave={this.selectWave}
             id={wave.id}
             key={wave.id}
